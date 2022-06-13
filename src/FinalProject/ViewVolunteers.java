a1610b5d4a1930892a4b715f7fd8bead6352c291
franz <franzlogan@gmail.com>
2022-06-13 16:04:51 -0400

    Added a ViewVolunteers Class with a single node (ListView) to be accessed from the account tab.

---
+++ src/FinalProject/ViewVolunteers.java
@@ -0,0 +1,27 @@
+package FinalProject;
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class ViewVolunteers {
+    ListView listView = new ListView();
+
+    public ViewVolunteers() {
+
+        Stage primaryStage = new Stage();
+        GridPane primaryPane = new GridPane();
+        primaryPane.setAlignment(Pos.CENTER);
+
+        primaryPane.add(listView, 0, 0);
+
+        Scene primaryScene = new Scene(primaryPane, 400, 500);
+        primaryStage.setScene(primaryScene);
+        primaryStage.setTitle("View Volunteers");
+        primaryStage.show();
+    }
+}
