d98460473c604ca7bd39a9ce5ce86aea15ce042c
franz <franzlogan@gmail.com>
2022-06-13 14:24:31 -0400

    Revision on application/sign-up window

--- src/FinalProject/SignUpWindow.java
+++ src/FinalProject/SignUpWindow.java
@@ -3,43 +3,92 @@ package FinalProject;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class SignUpWindow {
 
     // Sign up scene nodes
+    Label lblBarkApplication = new Label("Submit an application to become a BARK Volunteer!");
     TextField txtName = new TextField();
+    TextField txtDoB = new TextField();
     TextField txtEmail = new TextField();
+    TextField txtPhone = new TextField();
+    TextField txtEmergencyContactPhone = new TextField();
+    TextArea txtWorkHistory = new TextArea();
+    Label lblSpecializationPreference = new Label("Specialization Preference");
+    ComboBox cbSpecializationPreference = new ComboBox();
+    Label lblShadowPreference = new Label("Shadow Preference");
+    ComboBox cbShadowPreference = new ComboBox();
+    TextField txtAddress = new TextField();
+    TextField txtCity = new TextField();
+    TextField txtState = new TextField();
+    TextField txtZip = new TextField();
     TextField txtUsername = new TextField();
     TextField txtPassword = new TextField();
-    Button btnSignUp = new Button("Sign up");
+    Label spacer1 = new Label();
+    Label spacer2 = new Label();
+    Label spacer3 = new Label();
+    Button btnSignUp = new Button("Submit Application");
 
     public SignUpWindow(){
         Stage primaryStage = new Stage();
-        VBox vbox = new VBox(10);
-        vbox.setAlignment(Pos.CENTER);
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setVgap(5);
+        gridPane.setHgap(10);
 
         txtName.setPromptText("Name");
+        txtDoB.setPromptText("Date of birth");
         txtEmail.setPromptText("Email");
+        txtPhone.setPromptText("Phone Number");
+        txtEmergencyContactPhone.setPromptText("Emergency Contact Phone Number");
+        txtWorkHistory.setPromptText("Work History");
+        txtAddress.setPromptText("Street Address");
+        txtCity.setPromptText("City");
+        txtState.setPromptText("State");
+        txtZip.setPromptText("Zip Code");
         txtUsername.setPromptText("Username");
         txtPassword.setPromptText("Password");
-        vbox.getChildren().addAll(txtName, txtEmail, txtUsername, txtPassword, btnSignUp);
 
-        txtName.setMaxWidth(225);
-        txtEmail.setMaxWidth(225);
-        txtUsername.setMaxWidth(225);
-        txtPassword.setMaxWidth(225);
+        // formatting
+        setMaxWidth(txtWorkHistory, txtName, txtDoB, txtEmail, txtPhone,
+                txtEmergencyContactPhone,  txtAddress, txtCity, txtState, txtZip,txtUsername, txtPassword);
+        txtWorkHistory.setMaxHeight(150);
+        txtWorkHistory.setWrapText(true);
+        btnSignUp.setMinWidth(510);
 
-        txtName.setFocusTraversable(false);
-        txtEmail.setFocusTraversable(false);
-        txtUsername.setFocusTraversable(false);
-        txtPassword.setFocusTraversable(false);
+        setFocusTraversable(txtWorkHistory, txtName, txtDoB, txtEmail, txtPhone,
+                txtEmergencyContactPhone,  txtAddress, txtCity, txtState, txtZip,txtUsername, txtPassword);
 
-        Scene primaryScene = new Scene(vbox, 400, 500);
+        gridPane.add(lblBarkApplication, 0, 0, 2, 1);
+        lblBarkApplication.setStyle("-fx-font: 18 arial");
+        gridPane.add(txtName, 0, 1);
+        gridPane.add(txtDoB, 0, 2);
+        gridPane.add(txtEmail, 0, 3);
+        gridPane.add(txtPhone, 0, 4);
+        gridPane.add(txtAddress, 0, 5);
+        gridPane.add(txtCity, 0, 6);
+        gridPane.add(txtState, 0, 7);
+        gridPane.add(txtZip, 0, 8);
+        gridPane.add(spacer1, 0, 9);
+        gridPane.add(spacer2, 0, 10);
+        gridPane.add(txtWorkHistory, 1, 1, 2, 5);
+        gridPane.add(lblSpecializationPreference, 1, 6);
+        gridPane.add(cbSpecializationPreference, 2, 6);
+        gridPane.add(lblShadowPreference, 1, 7);
+        gridPane.add(cbShadowPreference, 2, 7);
+        gridPane.add(txtEmergencyContactPhone, 1, 8, 2, 1);
+
+        gridPane.add(txtUsername, 0, 11);
+        gridPane.add(txtPassword, 1, 11, 2, 1);
+        gridPane.add(spacer3, 0, 12);
+        gridPane.add(btnSignUp, 0, 13, 3, 1);
+
+        Scene primaryScene = new Scene(gridPane, 600, 600);
 
         primaryStage.setScene(primaryScene);
-        primaryStage.setTitle("Bark Sign Up");
+        primaryStage.setTitle("Bark Application");
         primaryStage.show();
 
         btnSignUp.setOnAction(e -> {
@@ -48,4 +97,16 @@ public class SignUpWindow {
         });
 
     }
+    public void setMaxWidth(TextArea ta, TextField... tf){
+        for (TextField t: tf)
+            t.setPrefWidth(250);
+        ta.setPrefWidth(250);
+    }
+
+    public void setFocusTraversable(TextArea ta, TextField... tf){
+        for (TextField t: tf)
+            t.setFocusTraversable(false);
+        ta.setFocusTraversable(false);
+    }
+
 }
