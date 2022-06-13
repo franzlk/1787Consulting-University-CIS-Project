6c57625f43649e172c4098fc261a2f0cf9ef6769
franz <franzlogan@gmail.com>
2022-06-12 23:03:44 -0400

    Various additions to VolunteerWindow removal of Scrolling class

--- src/FinalProject/VolunteerWindow.java
+++ src/FinalProject/VolunteerWindow.java
@@ -1,7 +1,5 @@
 package FinalProject;
 
-import javafx.application.Application;
-import javafx.event.*;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -10,6 +8,7 @@ import javafx.stage.Stage;
 
 public class VolunteerWindow {
     // home tab nodes
+    Button addPost = new Button("Add Post");
 
     // event tab nodes
 
@@ -40,58 +39,84 @@ public class VolunteerWindow {
         Stage primaryStage = new Stage();
 
         // TabPanes to be used in each Scene
-        TabPane volTabPane = new TabPane();
-        Tab volTab1 = new Tab("Home");
-        Tab volTab2 = new Tab("Event");
-        Tab volTab3 = new Tab("Time Entry");
-        Tab volTab4 = new Tab("Account");
+        TabPane TabPane = new TabPane();
+        Tab Tab1 = new Tab("Home");
+        Tab Tab2 = new Tab("Event");
+        Tab Tab3 = new Tab("Time Entry");
+        Tab Tab4 = new Tab("Account");
 
         // GridPanes to hold the nodes (buttons, labels) of each scene
-        GridPane volHomePane = new GridPane();
-        GridPane volEventPane = new GridPane();
-        GridPane volTimeEntryPane = new GridPane();
-        GridPane volAccountPane = new GridPane();
-        setPositionAlignment(volHomePane, volEventPane, volTimeEntryPane, volAccountPane);
+        //--------home tab
+        VBox content = new VBox(5);
+        ScrollPane scroller = new ScrollPane(content);
+        scroller.setFitToWidth(true);
+        BorderPane HomePane = new BorderPane(scroller, null, null, addPost, null);
+        //--------event tab
+        GridPane eventPane = new GridPane();
+        //--------account tab
+        GridPane timeEntryPane = new GridPane();
+        //--------home tab
+        GridPane accountPane = new GridPane();
 
-        volTab1.setContent(volHomePane);
-        volTab2.setContent(volEventPane);
-        volTab3.setContent(volTimeEntryPane);
-        volTab4.setContent(volAccountPane);
-        volTabPane.getTabs().addAll(volTab1, volTab2, volTab3, volTab4);
+        setPositionAlignment(timeEntryPane, accountPane);
 
-        // nodal content of volHomePane
+        //Tab1.setContent(HomePane);
+        Tab1.setContent(HomePane);
+        Tab2.setContent(eventPane);
+        Tab3.setContent(timeEntryPane);
+        Tab4.setContent(accountPane);
+        TabPane.getTabs().addAll(Tab1, Tab2, Tab3, Tab4);
 
-        // nodal content of volEventPane
+        // nodal content of HomePane
 
-        // nodal content of volTimeEntryPane
-        volTimeEntryPane.add(lblEventHours, 0, 0);
-        volTimeEntryPane.add(txtEventHours, 1, 0);
-        volTimeEntryPane.add(lblTotalMileage, 0, 1);
-        volTimeEntryPane.add(txtTotalMileage, 1, 1);
-        volTimeEntryPane.add(lblEventType, 0, 2);
-        volTimeEntryPane.add(cbEventType, 1, 2);
-        volTimeEntryPane.add(btnSubmit, 0, 3);
+        // nodal content of EventPane
 
-        // nodal content of volAccountPane
-        volAccountPane.add(lblMyAccountInfo, 0, 0);
+        // nodal content of TimeEntryPane
+        timeEntryPane.add(lblEventHours, 0, 0);
+        timeEntryPane.add(txtEventHours, 1, 0);
+        timeEntryPane.add(lblTotalMileage, 0, 1);
+        timeEntryPane.add(txtTotalMileage, 1, 1);
+        timeEntryPane.add(lblEventType, 0, 2);
+        timeEntryPane.add(cbEventType, 1, 2);
+        timeEntryPane.add(btnSubmit, 0, 3);
+
+        // nodal content of AccountPane
+        accountPane.add(lblMyAccountInfo, 0, 0);
         lblMyAccountInfo.setStyle("-fx-font: 18 arial");
-        volAccountPane.add(lblName, 0,1);
-        volAccountPane.add(txtName, 1, 1);
-        volAccountPane.add(lblEmail, 0, 2);
-        volAccountPane.add(txtEmail, 1, 2);
-        volAccountPane.add(lblAddress, 0, 3);
-        volAccountPane.add(txtAddress, 1, 3);
-        volAccountPane.add(lblSpecialization, 0, 4);
-        volAccountPane.add(cbSpecialization, 1, 4);
-        volAccountPane.add(btnEdit, 1, 5);
-        volAccountPane.add(lblAllVolunteers, 2, 0);
+        accountPane.add(lblName, 0,1);
+        accountPane.add(txtName, 1, 1);
+        accountPane.add(lblEmail, 0, 2);
+        accountPane.add(txtEmail, 1, 2);
+        accountPane.add(lblAddress, 0, 3);
+        accountPane.add(txtAddress, 1, 3);
+        accountPane.add(lblSpecialization, 0, 4);
+        accountPane.add(cbSpecialization, 1, 4);
+        accountPane.add(btnEdit, 1, 5);
+        accountPane.add(lblAllVolunteers, 2, 0);
         lblAllVolunteers.setStyle("-fx-font: 18 arial");
-        volAccountPane.add(listView, 2, 1, 1, 6);
+        accountPane.add(listView, 2, 1, 1, 6);
 
-        Scene primaryScene = new Scene(volTabPane, 600, 600);
+        Scene primaryScene = new Scene(TabPane, 600, 600);
         primaryStage.setScene(primaryScene);
         primaryStage.setTitle("Bark Volunteer View");
         primaryStage.show();
+
+
+        addPost.setOnAction(e -> {
+            AnchorPane anchorPane = new AnchorPane();
+            anchorPane.setStyle("-fx-background-color: WHITE");
+            Label label = new Label("Pane "+(content.getChildren().size()+1));
+            AnchorPane.setLeftAnchor(label, 5.0);
+            AnchorPane.setTopAnchor(label, 5.0);
+            Button button = new Button("Remove");
+            button.setOnAction(evt -> content.getChildren().remove(anchorPane));
+            AnchorPane.setRightAnchor(button, 5.0);
+            AnchorPane.setTopAnchor(button, 5.0);
+            AnchorPane.setBottomAnchor(button, 5.0);
+            anchorPane.getChildren().addAll(label, button);
+            content.getChildren().add(anchorPane);
+        });
+
 
 
     }
