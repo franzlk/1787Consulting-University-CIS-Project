17b35e205f09e497a00127395856735ff26844dd
franz <franzlogan@gmail.com>
2022-06-15 02:38:42 -0400

    set up GUI for the admin's account tab

--- src/FinalProject/AdminWindow.java
+++ src/FinalProject/AdminWindow.java
@@ -27,7 +27,35 @@ public class AdminWindow {
     Button btnDenyApp = new Button("Deny Selected Application");
 
     // account tab nodes
+    Label lblMyAccountInfo = new Label("My Account Info");
+    Label lblName = new Label("Name\t");
+    TextField txtName = new TextField();
+    Label lblDoB = new Label("Date of Birth\t");
+    TextField txtDoB = new TextField();
+    Label lblEmail = new Label("Email\t");
+    TextField txtEmail = new TextField();
+    Label lblPhone = new Label("Phone\t");
+    TextField txtPhone = new TextField();
+    Label lblAddress = new Label("Street Address\t");
+    TextField txtAddress = new TextField();
+    Label lblCity = new Label("City\t");
+    TextField txtCity = new TextField();
+    Label lblState = new Label("State\t");
+    TextField txtState = new TextField();
+    Label lblZip = new Label("Zip Code\t");
+    TextField txtZip = new TextField();
+    Label lblWorkHistory = new Label("Work History\t");
+    TextArea txtWorkHistory = new TextArea();
+    Label lblSpecialization = new Label("Specialization\t");
+    ComboBox cbSpecialization = new ComboBox();
+    Label lblShadow = new Label("Shadow name");
+    Label lblShadowName = new Label("Example Name");
+    Label lblEmergencyContactPhone = new Label("Emergency Contact Phone Number\t");
+    TextField txtEmergencyContactPhone = new TextField();
 
+    Button btnEdit = new Button("Confirm Account Changes");
+    Button btnLogout = new Button("Logout");
+    
 public AdminWindow(Volunteer activeUser){
     Stage primaryStage = new Stage();
 
@@ -85,7 +113,54 @@ public AdminWindow(Volunteer activeUser){
     volunteerGridPane.add(btnDenyApp, 3, 2);
 
     // nodal content of accountGridPane
+    // formatting
+    setMaxWidth(txtWorkHistory, txtName, txtDoB, txtEmail, txtPhone,
+            txtEmergencyContactPhone, txtAddress, txtCity, txtState, txtZip);
+    txtWorkHistory.setMaxHeight(150);
+    txtWorkHistory.setWrapText(true);
 
+    accountGridPane.add(lblMyAccountInfo, 0, 0, 2, 1);
+    lblMyAccountInfo.setStyle("-fx-font: 18 arial");
+    accountGridPane.add(lblName, 0, 1);
+    accountGridPane.add(txtName, 0, 2);
+    accountGridPane.add(lblDoB, 0, 3);
+    accountGridPane.add(txtDoB, 0, 4);
+    accountGridPane.add(lblEmail, 0, 5);
+    accountGridPane.add(txtEmail, 0, 6);
+    accountGridPane.add(lblPhone, 0, 7);
+    accountGridPane.add(txtPhone, 0, 8);
+    accountGridPane.add(lblAddress, 0, 9);
+    accountGridPane.add(txtAddress, 0, 10);
+    accountGridPane.add(lblCity, 0, 11);
+    accountGridPane.add(txtCity, 0, 12);
+    accountGridPane.add(lblState, 0, 13);
+    accountGridPane.add(txtState, 0, 14);
+    accountGridPane.add(lblZip, 0, 15);
+    accountGridPane.add(txtZip, 0, 16);
+    accountGridPane.add(lblWorkHistory, 1, 1, 3, 1);
+    accountGridPane.add(txtWorkHistory, 1, 2, 3, 5);
+    accountGridPane.add(lblSpecialization, 1, 7, 3, 1);
+    accountGridPane.add(cbSpecialization, 1, 8, 3, 1);
+    accountGridPane.add(lblShadow, 1, 9, 3, 1);
+    accountGridPane.add(lblShadowName, 1, 10, 3, 1);
+    accountGridPane.add(lblEmergencyContactPhone, 1, 11, 3, 1);
+    accountGridPane.add(txtEmergencyContactPhone, 1, 12, 3, 1);
+    accountGridPane.add(btnEdit, 1, 16);
+    accountGridPane.add(btnLogout, 3, 16);
+
+    txtName.setText(activeUser.getName());
+    txtDoB.setText(activeUser.getDateOfBirth());
+    txtEmail.setText(activeUser.getEmail());
+    txtPhone.setText(activeUser.getPhone());
+    txtAddress.setText(activeUser.getAddress());
+    txtCity.setText(activeUser.getCity());
+    txtState.setText(activeUser.getState());
+    txtZip.setText(activeUser.getZip());
+    txtWorkHistory.setText(activeUser.getWorkHistory());
+    //cbSpecialization.setText
+    //lblShadowName.setText();
+    txtEmergencyContactPhone.setText(activeUser.getEmergencyContactPhone());
+    
     Scene primaryScene = new Scene(TabPane, 775, 600);
     primaryStage.setScene(primaryScene);
     primaryStage.setTitle("BARK Admin View");
@@ -98,6 +173,12 @@ public AdminWindow(Volunteer activeUser){
             g.setHgap(10);
             g.setVgap(4);
         }
+    }
+
+    public void setMaxWidth(TextArea ta, TextField... tf) {
+        for (TextField t : tf)
+            t.setPrefWidth(250);
+        ta.setPrefWidth(250);
     }
 
     public void clearUpdateTable(ListView lv, ArrayList al, ObservableList obsList) {
