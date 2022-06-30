8450ad9e078faa899c1fecb3e3712eb0638cdbec
franz <franzlogan@gmail.com>
2022-06-30 09:59:48 -0400

    Added time history class

---
+++ src/FinalProject/TimeHistory.java
@@ -0,0 +1,56 @@
+package FinalProject;
+
+import java.util.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.io.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class TimeHistory {
+    static int count = 0;
+    int idNumber;
+    int volunteerID;
+    String date;
+    String timeIn;
+    String timeOut;
+
+    static ArrayList<TimeHistory> timeHistoryArrayList = new ArrayList<>();
+
+    // no pk constructor
+    public TimeHistory(int volunteerID, String date, String timeIn, String timeOut) {
+        this.idNumber = ++count;
+        this.volunteerID = volunteerID;
+        this.date = date;
+        this.timeIn = timeIn;
+        this.timeOut = timeOut;
+
+        timeHistoryArrayList.add(this);
+    }
+
+    // present pk constructor
+    public TimeHistory(int idNumber, int volunteerID, String date, String timeIn, String timeOut) {
+        this.idNumber = idNumber;
+        this.volunteerID = volunteerID;
+        this.date = date;
+        this.timeIn = timeIn;
+        this.timeOut = timeOut;
+    }
+
+    @Override
+    public String toString() {
+        return "TimeHistory{" +
+                "volunteerID=" + volunteerID +
+                ", date='" + date + '\'' +
+                ", timeIn='" + timeIn + '\'' +
+                ", timeOut='" + timeOut + '\'' +
+                '}';
+    }
+
+    public static void addTimeHistory(TimeHistory... timeHistories){
+        for (TimeHistory t : timeHistories){
+            Collections.addAll(timeHistoryArrayList, t);
+        }
+}
+
+}
